6910cd289eeefbbf8d28c9e787545bd52628b3ad
Javier Marquez <javi@arqex.com>
2018-04-26 18:01:43 +0200

    Fixes some event handlers

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,8 +5,8 @@ var ESCAPE_KEY = 27;
 var ENTER_KEY = 13;
 
 export default class TodoItem extends React.Component {
-	constructor( props ){
-		super( props );
+	constructor(props){
+		super(props);
 		this.state = {
 			editText: props.todo.title
 		};
@@ -33,6 +33,14 @@ export default class TodoItem extends React.Component {
 		this.props.todo.ui.set({ input: event.target.value }).now();
 	}
 
+	handleToggle() {
+		store.emit( 'todo:toggle', this.props.todo );
+	}
+
+	handleDelete(){
+		store.emit( 'todo:delete', this.props.todo );
+	}
+
 	/**
 	 * Safely manipulate the DOM after updating the state when invoking
 	 * `this.props.onEdit()` in the `handleEdit` method above.
@@ -42,7 +50,7 @@ export default class TodoItem extends React.Component {
 	componentDidUpdate (prevProps) {
 		var status = this.props.todo.ui.status;
 		if (prevProps.todo.ui.status != status && status == 'editing') {
-			var node = React.findDOMNode(this.refs.editField);
+			var node = this.refs.editField;
 			node.focus();
 			node.setSelectionRange(node.value.length, node.value.length);
 		}
@@ -64,9 +72,9 @@ export default class TodoItem extends React.Component {
 						ref="editField"
 						className="edit"
 						value={ todo.ui.input }
-						onBlur={this.handleUpdate}
-						onChange={this.handleChange}
-						onKeyDown={this.handleKeyDown} />
+						onBlur={ () => this.handleUpdate() }
+						onChange={ e => this.handleChange(e) }
+						onKeyDown={ e => this.handleKeyDown(e) } />
 					<span className="loadingMessage">Saving...</span>
 				</div>
 			);
@@ -77,11 +85,11 @@ export default class TodoItem extends React.Component {
 					<input className="toggle"
 						type="checkbox"
 						checked={ todo.model.completed }
-						onChange={ () => store.emit( 'todo:toggle', this.props.todo ) } />
-					<label onDoubleClick={this.handleEdit}>
+						onChange={ () => this.handleToggle() } />
+					<label onDoubleClick={ () => this.handleEdit() }>
 						{ todo.model.title }
 					</label>
-					<button className="destroy" onClick={ () => store.emit( 'todo:delete', this.props.todo ) } />
+					<button className="destroy" onClick={ () => this.handleDelete() } />
 					<span className="loadingMessage">Deleting...</span>
 				</div>
 			)
